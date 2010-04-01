c19d77bc7451b111eecb9062863afd2782dfaddf
Roman Kharitonov <refaim.vl@gmail.com>
2010-04-01 23:40:20 +1100

    Version 0.6

--- src/const.py
+++ src/const.py
@@ -10,7 +10,7 @@ APP_URL = 'http://github.com/refaim/reposeer'
 APP_LONG_NAME = 'Reposeer'
 APP_SHORT_NAME = 'rs'
 
-APP_VERSION = 0.5
+APP_VERSION = 0.6
 APP_VERSION_STRING = '{0} {1}\nby {2} ({3})'.format(
     APP_LONG_NAME, APP_VERSION, APP_AUTHOR, APP_AUTHOR_MAIL)
 
