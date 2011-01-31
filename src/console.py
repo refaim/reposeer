452265e62d8a90e1d6f1279f36b0aa9c890274f4
Roman Kharitonov <refaim.vl@gmail.com>
2011-01-31 18:22:33 +1000

    Add function to determine actual size of terminal in Windows

--- src/console.py
+++ src/console.py
@@ -1,6 +1,40 @@
 ﻿# -*- coding: utf-8 -*-
 
+import sys
+
 def getTerminalSize():
+    if 'win32' in sys.platform:
+        return _win32_get_terminal_size()
+    else:
+        return _unix_get_terminal_size()
+
+def getTerminalWidth():
+    return getTerminalSize()[0]
+
+def getTerminalHeight():
+    return getTerminalSize()[1]
+
+def _win32_get_terminal_size():
+    # http://code.activestate.com/recipes/440694-determine-size-of-console-window-on-windows/
+    from ctypes import windll, create_string_buffer
+    # stdin handle is -10
+    # stdout handle is -11
+    # stderr handle is -12
+    h = windll.kernel32.GetStdHandle(-12)
+    csbi = create_string_buffer(22)
+    res = windll.kernel32.GetConsoleScreenBufferInfo(h, csbi)
+    if res:
+        import struct
+        (bufx, bufy, curx, cury, wattr,
+         left, top, right, bottom, maxx, maxy) = struct.unpack("hhhhHhhhhhh", csbi.raw)
+        sizex = right - left + 1
+        sizey = bottom - top + 1
+    else:
+        sizex, sizey = 80, 25 # can't determine actual size - return default values
+    return sizex, sizey
+
+def _unix_get_terminal_size():
+
     def ioctl_GWINSZ(fd):
         try:
             import fcntl, termios, struct, os
@@ -9,6 +43,7 @@ def getTerminalSize():
         except:
             return None
         return cr
+
     cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
     if not cr:
         try:
@@ -23,9 +58,3 @@ def getTerminalSize():
         except:
             cr = (25, 80)
     return int(cr[1]), int(cr[0])
-
-def getTerminalWidth():
-    return getTerminalSize()[0]
-
-def getTerminalHeight():
-    return getTerminalSize()[1]
